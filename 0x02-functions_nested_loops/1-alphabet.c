4337b0874169d5ead45e45c2a9af42fbfd0b47b9
McDaniels1 <mcdaniels4real@gmail.com>
2022-09-13 22:31:39 -0700

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  *
  * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c;
 
-	for (c = 'a' c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++;)
 	{
 		_putchar(0);
 	}
