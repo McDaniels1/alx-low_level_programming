efe5b221a70fe42dfa81023f6e68c4837219ba79
McDaniels1 <mcdaniels4real@gmail.com>
2022-09-13 18:43:03 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include"main.h"
+
+/**
+ * print_sign - print + if n is greater than zero,
+ *             0 if n is zero and - if n is less
+ *             thanm zero.
+ *
+ * @n: takes i nteger type input for function.
+ *
+ * Return: 1 if +, 0 if 0 and -1 if - 
+ */
+
+int pri nt_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1)'
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		return (-1);
+	}
+}
