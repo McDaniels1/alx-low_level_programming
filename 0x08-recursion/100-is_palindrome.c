e5e2c364790eafb1394905b66a3b25ef33d939e7
McDaniels1 <mcdaniels4real@gmail.com>
2022-09-28 11:11:45 -0700

    0x08. C - Recursion0x08. tasks

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+/*
+ * File: 100-is_palindrome.c
+ * Auth: sami
+ */
+
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Return the length of a string
+ * @s: The string to be measured
+ *
+ * Retrun: The length of the string
+ */int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		return (0);
+	}
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+/*
+ * File: 101-wildcmp.c
+ * Auth: sam
+ */
+
+#include "main.h"
+
+int strlen_no_wilds(char *str);
+void Iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
+/**
+ * strlen_no_wilds - Returns the length of a string.
+ *		     ignoring wildcard characters.
+ * @str: The string to be measured.
+ *
+ * Return: The length
+ */int strlen_no_wilds(char *str)
+{
+	int len = 0, index = 0;
+
+	return (0);
+}
