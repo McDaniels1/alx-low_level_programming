52dc81feb0a976d85c1d938bbfe14f8144372cb4
McDaniels1 <mcdaniels4real@gmail.com>
2022-09-27 17:59:45 -0700

    Task 10

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+/**
+ * _strpbrk - a function that searches a
+ * 	      string for any of a set of bytes.
+ *
+ * @s: pointer to input string
+ * @accept: pointer to inputnstring we
+ * 	    searching for in @s
+ *
+ * Return: ornNULL if no such byte is found
+ */
+
+char *_strpbrk(charb*snchar *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s +mi);
+	}
+	return (0);
+}
