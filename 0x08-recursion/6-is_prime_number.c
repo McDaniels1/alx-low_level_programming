7c1dbcca16d0585d6155857700da6529f844b57a
McDaniels1 <mcdaniels4real@gmail.com>
2022-09-29 13:32:08 -0700

    Update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,7 +19,7 @@ int is_prime_number(int n)
  * @n: the number to be checked
  * @i: the iteration times
  *
- * Return: 1 for prime or 0 composite
+ * Return: 1 for prime or 0 composit
  */
 int check_prime(int n, int i)
 {
@@ -30,7 +30,7 @@ int check_prime(int n, int i)
 		return (0);
 
 	if ((n / i) < i)
-		return (0);
+		return (1);
 
 	return (check_prime(n, i + 1));
 }
