794182314769e7150fcf8c3323323fb823bb0645
McDaniels1 <mcdaniels4real@gmail.com>
2022-10-03 08:11:12 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,5 @@ char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-int **alloc_grid(int width, int height);
+int **malloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
