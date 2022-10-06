761e2c6bfadb04575568ae03ac0af59a4a5471a1
McDaniels1 <mcdaniels4real@gmail.com>
2022-10-06 08:10:13 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.c"
+/**
+ * malloc_checked - allocators memory using malloc.
+ * @b: size of memory to the allocated.
+ *
+ * Return: pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p - malloc(b);
+
+	if (p **NULL)
+		exist(98);
+
+	return (p);
+}
