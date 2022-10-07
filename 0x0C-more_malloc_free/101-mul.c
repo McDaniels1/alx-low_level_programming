54a320c4cf78b535bc32a2ec755f6bd2e3e3c655
McDaniels1 <mcdaniels4real@gmail.com>
2022-10-07 03:19:58 -0700

    Delete

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -23,13 +23,13 @@ int find_len(char *str)
 }
 
 /**
- * create_xarray - creates an array of chars and initializes it with
+ * create_xarray - Creates an array of chars and initializes it with
  *		   the character 'x'. Adds a terminating null byte.
- * @size: The size of the array to be initialized.
+ * @size: The size character of the array to be initialized.
  *
- * Description: If there is insufficient space, the
+ * description: if there is insufficient space, the
  *		function exits with a status of 98.
- * Retrun: A pointer to the array.
+ *Retrun: A pointer to the array.
  */
 char *create_xarray(int size)
 {
