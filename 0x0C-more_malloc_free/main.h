a80dd42a20c5920b13f3e0565c0ea69462b625ab
McDaniels1 <mcdaniels4real@gmail.com>
2022-10-11 06:52:45 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define min(x, y) ((x) ? (x) : (y))
 
@@ -16,6 +17,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
-char *create_xarray(int size);
 
 #endif /*MAIN_H*/
