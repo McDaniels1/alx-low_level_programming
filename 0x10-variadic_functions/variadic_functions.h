2b417244483aa53d5dfb24f1cbb9ef0b3ac4d31d
McDaniels1 <mcdaniels4real@gmail.com>
2022-10-15 00:58:06 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,7 +9,6 @@ typedef struct printer
 
 } printer_t;
 
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
